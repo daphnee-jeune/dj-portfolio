82667a57d7defd7134c25796b5e01952008e0e65
Daphnee Jeune <daphneejeune@Daphnees-MacBook-Pro.local>
2024-08-12 15:51:19 -0500

    add 'see more' logic on project cards

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaMapMarker } from 'react-icons/fa'
 
 const ProjectCard = ({ project }) => {
+  const [showFullDesc, setshowFullDesc] = useState(false);
+
+  let description = project.description;
+  if (!showFullDesc) {
+    description = description.substring(0, 90) + "...";
+  }
   return (
     <div className="bg-white rounded-xl shadow-md relative">
       <div className="p-4">
@@ -9,15 +16,20 @@ const ProjectCard = ({ project }) => {
           <h3 className="text-xl font-bold">{project.title}</h3>
         </div>
 
-        <div className="mb-5">{project.description}</div>
-
+        <div className="mb-5">{description}</div>
+        <button
+          className="text-indigo-500 mb-5 hover:text-indigo-600"
+          onClick={() => setshowFullDesc((prev) => !prev)}
+        >
+          {showFullDesc ? "Less" : "More"}
+        </button>
         <h3 className="text-indigo-500 mb-2">{project.salary}</h3>
 
         <div className="border border-gray-100 mb-5"></div>
 
         <div className="flex flex-col lg:flex-row justify-between mb-4">
           <div className="text-orange-700 mb-3">
-            <i className="fa-solid fa-location-dot text-lg"></i>
+            <FaMapMarker className="inline text-lg mb-1 mr-1" />
             {project.location}
           </div>
           <a
